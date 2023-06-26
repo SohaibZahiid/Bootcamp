af112a61f4bab8e74a99f5b6d1504bf300d105d4
Sohaib Zahid <sohaib642@gmail.com>
2023-06-26 19:15:34 +0200

    adding localstorage to pokemon app

--- Pokemon/script.js
+++ Pokemon/script.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
     const API = "https://pokeapi.co/api/v2/pokemon?limit=50";
     let ALL_POKEMONS;
-    let ORIGINAL_POKEMONS; // Store the original list of Pokémon data
+    let ALL_POKEMONS_DETAILS = [];
     let CURRENT_PAGE = 1;
     const ITEMS_PER_PAGE = 8;
 
@@ -11,57 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const deletePokemon = (name) => {
         ALL_POKEMONS = ALL_POKEMONS.filter((p) => p.name != name);
-        ORIGINAL_POKEMONS = ORIGINAL_POKEMONS.filter((p) => p.name != name); // Update the original list as well
         paginatePokemons();
-    };
-
-    const getPokemons = async () => {
-        try {
-            const response = await fetch(API);
-            const data = await response.json();
-            ALL_POKEMONS = data.results;
-            ORIGINAL_POKEMONS = data.results; // Store the original list
-            paginatePokemons();
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    const fetchPokemonDetails = async (url) => {
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            return data;
-        } catch (err) {
-            throw new Error("Failed to fetch Pokémon details");
-        }
-    };
-
-    const createPokemonElement = (id, name, image) => {
-        const pokemonEl = document.createElement("div");
-        pokemonEl.classList.add("pokemon");
-        pokemonEl.setAttribute("data-id", id);
-
-        const html = `
-        <h4 class="name">${name}</h4>
-        <button class="btn delete" data-name="${name}">Delete</button>
-        <img src="${image}" alt="" class="image">
-      `;
-        pokemonEl.innerHTML = html;
-
-        const deleteBtn = pokemonEl.querySelector(".delete");
-        deleteBtn.addEventListener("click", (e) => {
-            const pokemonName = e.target.dataset.name;
-            deletePokemon(pokemonName);
-        });
-
-        return pokemonEl;
-    };
-
-    const addPokemonClickListener = (pokemonEl, pokemonId) => {
-        pokemonEl.addEventListener("click", () => {
-            window.location.href = `pokemon-details.html?id=${pokemonId}`;
-        });
     };
 
     const paginatePokemons = () => {
@@ -73,14 +23,24 @@ window.addEventListener("DOMContentLoaded", () => {
         const paginatedPokemons = ALL_POKEMONS.slice(startIndex, endIndex);
 
         paginatedPokemons.forEach(async (pokemon) => {
-            const pokemonDetails = await fetchPokemonDetails(pokemon.url);
-            const pokemonEl = createPokemonElement(
-                pokemonDetails.id,
-                pokemon.name,
-                pokemonDetails.sprites.front_default
+            const pokemonDetails = ALL_POKEMONS_DETAILS.find(
+                (p) => p.id === pokemon.id
             );
-            pokemonContainerEl.appendChild(pokemonEl);
-            addPokemonClickListener(pokemonEl.querySelector("img"), pokemonDetails.id);
+
+            if (pokemonDetails) {
+                const pokemonEl = createPokemonDOM(
+                    pokemonDetails.id,
+                    pokemonDetails.name,
+                    pokemonDetails.image
+                );
+                pokemonContainerEl.appendChild(pokemonEl);
+                addPokemonClickListener(
+                    pokemonEl.querySelector("img"),
+                    pokemonDetails.id
+                );
+            } else {
+                await getPokemonDetails(pokemon);
+            }
         });
 
         generatePaginationButtons();
@@ -137,273 +97,130 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     };
 
+    const displayFilteredPokemons = (filteredPokemons) => {
+        pokemonContainerEl.innerHTML = "";
+
+        ALL_POKEMONS_DETAILS = JSON.parse(
+            localStorage.getItem("pokemons-details")
+        );
+        filteredPokemons.forEach((pokemon) => {
+            const pokemonEl = createPokemonDOM(
+                pokemon.id,
+                pokemon.name,
+                pokemon.image
+            );
+            pokemonContainerEl.appendChild(pokemonEl);
+        });
+
+        generatePaginationButtons();
+    };
+
     searchEl.addEventListener("input", async (e) => {
         const searchValue = e.target.value.trim().toLowerCase();
 
         if (searchValue) {
-            const filteredPokemons = ORIGINAL_POKEMONS.filter((pokemon) =>
-                pokemon.name.toLowerCase().includes(searchValue)
+            const filteredPokemons = ALL_POKEMONS_DETAILS.filter((pokemon) =>
+                pokemon.name.trim().toLowerCase().includes(searchValue)
             );
-
-            ALL_POKEMONS = filteredPokemons;
-            CURRENT_PAGE = 1;
-            paginatePokemons();
+            displayFilteredPokemons(filteredPokemons);
         } else {
-            ALL_POKEMONS = ORIGINAL_POKEMONS; // Restore the original list
-            CURRENT_PAGE = 1; // Reset the current page to 1
-            paginatePokemons();
+            getAllPokemons();
         }
     });
 
-    getPokemons();
+    const createPokemonDOM = (id, name, image) => {
+        const pokemonEl = document.createElement("div");
+        pokemonEl.classList.add("pokemon");
+        pokemonEl.setAttribute("data-id", id);
+
+        const html = `
+          <h4 class="name">${name}</h4>
+          <button class="btn delete " data-name="${name}">X</button>
+          <img src="${image}" alt="" class="image">
+        `;
+        pokemonEl.innerHTML = html;
+
+        const deleteBtn = pokemonEl.querySelector(".delete");
+        deleteBtn.addEventListener("click", (e) => {
+            const pokemonName = e.target.dataset.name;
+            deletePokemon(pokemonName);
+        });
+
+        const imageEl = pokemonEl.querySelector(".image");
+        imageEl.addEventListener("click", () => {
+            window.location.href = `pokemon-details.html?id=${id}`;
+        });
+
+        return pokemonEl;
+    };
+
+    const getPokemonDetails = async (pokemon) => {
+        try {
+            const res = await fetch(pokemon.url);
+            const data = await res.json();
+            const pokemonData = {
+                id: data.id,
+                name: data.name,
+                order: data.order,
+                image: data.sprites.front_default,
+            };
+            ALL_POKEMONS_DETAILS.push(pokemonData);
+            const pokemonEl = createPokemonDOM(
+                data.id,
+                data.name,
+                data.sprites.front_default
+            );
+            pokemonContainerEl.appendChild(pokemonEl);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const checkLocalStorage = (key) => {
+        return localStorage.getItem(key) !== null;
+    };
+
+    const getAllPokemons = async () => {
+        CURRENT_PAGE = 1; // Reset CURRENT_PAGE to 1
+
+        if (!checkLocalStorage("pokemons")) {
+            try {
+                const res = await fetch(API);
+                const data = await res.json();
+                ALL_POKEMONS = data.results;
+                localStorage.setItem("pokemons", JSON.stringify(ALL_POKEMONS));
+            } catch (error) {
+                console.log(error);
+                return;
+            }
+        } else {
+            ALL_POKEMONS = JSON.parse(localStorage.getItem("pokemons"));
+        }
+
+        if (!checkLocalStorage("pokemons-details")) {
+            for (const pokemon of ALL_POKEMONS) {
+                await getPokemonDetails(pokemon);
+            }
+            localStorage.setItem(
+                "pokemons-details",
+                JSON.stringify(ALL_POKEMONS_DETAILS)
+            );
+        } else {
+            ALL_POKEMONS_DETAILS = JSON.parse(
+                localStorage.getItem("pokemons-details")
+            );
+            for (const pokemonData of ALL_POKEMONS_DETAILS) {
+                const pokemonEl = createPokemonDOM(
+                    pokemonData.id,
+                    pokemonData.name,
+                    pokemonData.image
+                );
+                pokemonContainerEl.appendChild(pokemonEl);
+            }
+        }
+
+        paginatePokemons();
+    };
+
+    getAllPokemons();
 });
-
-
-// window.addEventListener("DOMContentLoaded", () => {
-//     const API = "https://pokeapi.co/api/v2/pokemon?limit=50";
-
-//     let ALL_POKEMONS;
-
-//     const pokemonContainerEl = document.querySelector(".pokemon-container");
-//     const searchEl = document.querySelector(".search");
-
-//     const getPokemons = async () => {
-//         try {
-//             const response = await fetch(API);
-//             const data = await response.json();
-//             const pokemons = data.results;
-//             ALL_POKEMONS = pokemons;
-
-//             for (const pokemon of pokemons) {
-//                 const pokemonDetails = await fetchPokemonDetails(pokemon.url);
-//                 const pokemonEl = createPokemonElement(
-//                     pokemonDetails.id,
-//                     pokemon.name,
-//                     pokemonDetails.sprites.front_default
-//                 );
-//                 pokemonContainerEl.appendChild(pokemonEl);
-//                 addPokemonClickListener(pokemonEl, pokemonDetails.id);
-//             }
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     };
-
-//     const fetchPokemonDetails = async (url) => {
-//         try {
-//             const response = await fetch(url);
-//             const data = await response.json();
-//             return data;
-//         } catch (err) {
-//             throw new Error("Failed to fetch Pokémon details");
-//         }
-//     };
-
-//     const createPokemonElement = (id, name, image) => {
-//         const pokemonEl = document.createElement("div");
-//         pokemonEl.classList.add("pokemon");
-//         pokemonEl.setAttribute("data-id", id);
-
-//         const html = `
-//             <h4 class="name">${name}</h4>
-//             <img src="${image}" alt="" class="image">
-//         `;
-//         pokemonEl.innerHTML = html;
-
-//         return pokemonEl;
-//     };
-
-//     const addPokemonClickListener = (pokemonEl, pokemonId) => {
-//         pokemonEl.addEventListener("click", () => {
-//             window.location.href = `pokemon-details.html?id=${pokemonId}`;
-//         });
-//     };
-
-//     searchEl.addEventListener("input", async (e) => {
-//         const searchValue = e.target.value.trim().toLowerCase();
-
-//         if (searchValue) {
-//             const filteredPokemons = ALL_POKEMONS.filter((pokemon) =>
-//                 pokemon.name.toLowerCase().includes(searchValue)
-//             );
-//             pokemonContainerEl.innerHTML = "";
-
-//             console.log(filteredPokemons);
-
-//             pokemonContainerEl.innerHTML = "";
-
-//             filteredPokemons.forEach(async (pokemon) => {
-//                 const pokemonDetails = await fetchPokemonDetails(
-//                     `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-//                 );
-
-//                 const pokemonEl = createPokemonElement(
-//                     pokemonDetails.id,
-//                     pokemonDetails.name,
-//                     pokemonDetails.sprites.front_default
-//                 );
-//                 pokemonContainerEl.appendChild(pokemonEl);
-//                 addPokemonClickListener(pokemonEl, pokemonDetails.id);
-//             });
-//         } else {
-//             getPokemons();
-//         }
-//     });
-
-//     getPokemons();
-
-//     const ul = document.querySelector("ul");
-
-//     function elem(allPages, page) {
-//         let li = "";
-
-//         let beforePages = page - 1;
-//         let afterPages = page + 1;
-//         let liActive;
-
-//         if (page > 1) {
-//             li += `<li class="btn prev"><i class="fas fa-angle-left"></i></li>`;
-//         }
-
-//         for (
-//             let pageLength = beforePages;
-//             pageLength <= afterPages;
-//             pageLength++
-//         ) {
-//             if (pageLength > allPages) {
-//                 continue;
-//             }
-
-//             if (pageLength === 0) {
-//                 pageLength++;
-//             }
-
-//             if (page === pageLength) {
-//                 liActive = "active";
-//             } else {
-//                 liActive = "";
-//             }
-
-//             li += `<li class="num ${liActive} specButton" data-page="${pageLength}"><span>${pageLength}</span></li>`;
-//         }
-
-//         if (page < allPages) {
-//             li += `<li class="btn next"><i class="fas fa-angle-right"></i></li>`;
-//         }
-
-//         ul.innerHTML = li;
-
-//         const prevButton = document.querySelector(".prev");
-//         const nextButton = document.querySelector(".next");
-//         const activeButtons = document.querySelectorAll(".specButton");
-
-//         if (prevButton) {
-//             prevButton.addEventListener("click", () => {
-//                 elem(allPages, page - 1);
-//             });
-//         }
-
-//         if (nextButton) {
-//             nextButton.addEventListener("click", () => {
-//                 elem(allPages, page + 1);
-//             });
-//         }
-
-//         if (activeButtons) {
-//             activeButtons.forEach((button) => {
-//                 button.addEventListener("click", () => {
-//                     const selectedPage = parseInt(
-//                         button.getAttribute("data-page")
-//                     );
-//                     elem(allPages, selectedPage);
-//                 });
-//             });
-//         }
-//     }
-
-//     elem(12, 2)
-
-// });
-
-// window.addEventListener("DOMContentLoaded", () => {
-//     const API = "https://pokeapi.co/api/v2/pokemon?limit=50";
-
-//     let pokemonContainerEl = document.querySelector(".pokemon-container");
-//     const searchEl = document.querySelector(".search");
-
-//     const getPokemons = () => {
-//         fetch(API)
-//             .then((res) => res.json())
-//             .then((pokemons) => {
-//                 pokemons.results.forEach((pokemon) => {
-//                     fetch(pokemon.url)
-//                         .then((res) => res.json())
-//                         .then((p) => {
-//                             const pokemonEl = createPokemon(
-//                                 p.id,
-//                                 pokemon.name,
-//                                 p.sprites.front_default
-//                             );
-//                             pokemonContainerEl.appendChild(pokemonEl);
-//                             addPokemonClickListener(pokemonEl, p.id);
-//                         });
-//                 });
-//             })
-//             .catch((err) => console.log(err));
-//     };
-
-//     const createPokemon = (id, name, image) => {
-//         const pokemonEl = document.createElement("div");
-//         pokemonEl.classList.add("pokemon");
-//         pokemonEl.setAttribute("data-id", id);
-
-//         const html = `
-//         <h4 class="name">${name}</h4>
-//         <img src="${image}" alt="" class="image">
-//         `;
-//         pokemonEl.innerHTML = html;
-
-//         return pokemonEl;
-//     };
-
-//     const addPokemonClickListener = (pokemonEl, pokemonId) => {
-//         pokemonEl.addEventListener("click", () => {
-//             //   const pokemonId = pokemonEl.getAttribute("data-id");
-//             //   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-//             //   .then(res => res.json())
-//             //   .then(data => {
-//             //     console.log(data);
-//             //   })
-
-//             window.location.href = `pokemon-details.html?id=${pokemonId}`;
-//         });
-//     };
-
-//     getPokemons();
-
-//     searchEl.addEventListener("input", (e) => {
-//         if (e.target.value) {
-//             const input = e.target.value.toLowerCase();
-//             fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
-//                 .then((res) => res.json())
-//                 .then((p) => {
-//                     if (p) {
-//                         pokemonContainerEl.innerHTML = "";
-//                         const pokemonEl = createPokemon(
-//                             p.id,
-//                             p.name,
-//                             p.sprites.front_default
-//                         );
-//                         pokemonContainerEl.appendChild(pokemonEl);
-//                         addPokemonClickListener(pokemonEl, p.id);
-//                     } else {
-//                         return
-//                     }
-//                 }).catch((err) => {
-//                     console.log(err.message);
-//                 });
-//         } else {
-//             getPokemons()
-//         }
-//     });
-// });
